84601d02b44a0d09a1ddb467b59423203c06840b
Ednaldo Gonçalves <ednaldo.tic@gmail.com>
2024-01-21 14:03:44 -0500

    Update rmse.py

--- assignment2/rmse.py
+++ assignment2/rmse.py
@@ -3,5 +3,7 @@ import numpy as np
 def rmse(predictions, targets):
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = None # TODO: Implement RMSE Calculation here...
+    #rmse = None # TODO: Implement RMSE Calculation here...
+    n = len(tar)
+    rmse = np.sqrt(np.sum((pred-tar)**2)/n)
     return rmse
